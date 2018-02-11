936f522fe6b3021ba84bad434346108a5c6e5108
Administrator@XTM-01701112049 <502583304@qq.com>
2018-02-11 23:26:40 +0800

    编写header,tab组件

---
+++ src/components/m-header/m-header.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+// import './m-header.less';
+
+export default class MHeader extends Component {
+    render() {
+        return (
+            <div className="m-header">
+                <div className="icon"></div>
+                <h1 className="text">Albert Music</h1>
+                <Link to="/user" className="mine">
+                    <i class="icon-mine" />
+                </Link>
+            </div>
+        );
+    }
+}
---
+++ src/components/tab/tab.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+// import './m-header.less';
+
+export default class MHeader extends Component {
+    render() {
+        return (
+            <div className="tab">
+
+            </div>
+        );
+    }
+}
