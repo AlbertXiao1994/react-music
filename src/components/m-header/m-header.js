24cb9c83fd2e24a8cad85ba287981368af038dcb
Administrator@XTM-01701112049 <502583304@qq.com>
2018-02-12 13:58:10 +0800

    改变图片资源路径

--- src/components/m-header/m-header.js
+++ src/components/m-header/m-header.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import './m-header.less';
 
 export default class MHeader extends Component {
@@ -8,9 +8,9 @@ export default class MHeader extends Component {
             <div className="m-header">
                 <div className="icon"></div>
                 <h1 className="text">Albert Music</h1>
-                <Link to="/user" className="mine">
-                    <i class="icon-mine" />
-                </Link>
+                <a className="mine">
+                    <i className="icon-mine" />
+                </a>
             </div>
         );
     }
