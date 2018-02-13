244032cd08533218b05c33bd5a7b25132a1a9593
Administrator@XTM-01701112049 <502583304@qq.com>
2018-02-13 10:17:16 +0800

    引入Recommend组件的样式

--- src/components/recommend/recommend.js
+++ src/components/recommend/recommend.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-export default class RouterConfig extends Component {
+export default class Recommend extends Component {
     render() {
         return (
             <div>
