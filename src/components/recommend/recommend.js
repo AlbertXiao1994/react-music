1e7f71dc8dfb60ab36004280fa6b752fcb3490e4
Administrator@XTM-01701112049 <502583304@qq.com>
2018-02-13 11:00:23 +0800

    搭建recommend组件页面布局；引入Slider组件

--- src/components/recommend/recommend.js
+++ src/components/recommend/recommend.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
+import Slider from 'base/slider/slider';
 
 export default class Recommend extends Component {
-    render() {
-        return (
-            <div>
-                recommend
+  state = {
+    recommends: []
+  };
+  render() {
+    const { recommends } = this.state;
+    const recommendItems = recommends.map((item) => 
+      <div>
+        <a href={item.linkUrl}>
+          <img src={item.picUrl} className="needsClick" alt="" />
+        </a>
+      </div>
+    );
+    return (
+      <div>
+        <div className="recommend-content">
+          <div>
+            <div className="slider-wrapper">
+              <Slider>
+                {recommendItems}
+              </Slider>
             </div>
-        );
-    }
+            <div className="recommend-list">
+              <h1 class="list-title">热门歌单推荐</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
