a9cc0dec77b40e4b658a7688621fb273246dc817
Administrator@XTM-01701112049 <502583304@qq.com>
2018-02-15 18:20:03 +0800

    更正类名属性

--- src/base/loading/loading.js
+++ src/base/loading/loading.js
@@ -6,8 +6,8 @@ export default class Loading extends Component {
     render() {
         return (
             <div className="loading">
-                <img width="24" height="24" src="./loading.gif" alt=""/>>
-                <p class="desc">{this.props.title}</p>
+                <img width="24" height="24" src="./loading.gif" alt=""/>
+                <p className="desc">{this.props.title}</p>
             </div>
         );
     }
