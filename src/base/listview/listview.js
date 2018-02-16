13747221f29658880ce924a7d8d7d020866c40c0
Administrator@XTM-01701112049 <502583304@qq.com>
2018-02-16 21:53:20 +0800

    实现导航与滚动联动

--- src/base/listview/listview.js
+++ src/base/listview/listview.js
@@ -3,9 +3,10 @@ import ProbeTypes from 'prop-types';
 import Scroll from 'base/scroll/scroll';
 import Loading from 'base/loading/loading';
 import { getAttr } from 'common/js/dom';
+import { is, fromJS } from 'immutable';
 import './listview.less';
 const AHCHOR_HEIGHT = 18;
-// const TITLE_HEIGHT = 30;
+const TITLE_HEIGHT = 30;
 
 export default class ListView extends Component {
     state = {
@@ -20,6 +21,7 @@ export default class ListView extends Component {
         this.listenScroll = true
         this.probeType = 3
         this.listGroup = []
+        this.fixedTitle = ''
     }
     componentWillReceiveProps(nextProps) {
         if (nextProps.data !== this.props.data) {
@@ -28,13 +30,20 @@ export default class ListView extends Component {
             setTimeout(() => {
                 this._calListHeight()
             }, 20)
-            // console.log(this.listHeight)
         }
+    }
+    shouldComponentUpdate(nextProps, nextState) {
+        if (nextState.currentIndex !== this.state.currentIndex) {
+            this.handleFixTitleChange(nextState.currentIndex)
+        }
+        return !is(fromJS(this.props), fromJS(nextProps)) || !is(fromJS(this.state),fromJS(nextState))
     }
     createMarkup = (item) => {
 		return {__html: item.name};
     }
     shortcutToggle = (e) => {
+        e.preventDefault();
+        e.stopPropagation();
         let anchorIndex = getAttr(e.target, 'index');
         anchorIndex = parseInt(anchorIndex, 10)
 
@@ -56,7 +65,6 @@ export default class ListView extends Component {
         let { scrollY } = this.state;
         if (scrollY > 0) {
             this.setState({currentIndex: 0})
-            // console.log(this.state.currentIndex)
             return;
           }
           // 当在中部
@@ -65,14 +73,26 @@ export default class ListView extends Component {
             let height2 = this.listHeight[i + 1]
             if (-scrollY >= height1 && -scrollY < height2) {
               this.setState({currentIndex: i})
-            //   console.log(this.state.currentIndex)
-            //   this.diff = height2 + scrollY
+              this.diff = height2 + scrollY
+              this.handleDiffChange(this.diff)
               return;
             }
           }
           // 当在底部
           this.setState({currentIndex: this.listHeight.length - 2})
-        //   console.log(this.state.currentIndex)
+          this.handleFixTitleChange(this.state.currentIndex)
+    }
+    handleDiffChange = (newVal) => {
+        let offset = (newVal < TITLE_HEIGHT && newVal > 0) ? TITLE_HEIGHT - newVal : 0
+        if (this.offset === offset) {
+          return;
+        }
+        this.offset = offset
+        this.topFixed.style.transform = `translate3d(0,${-offset}px,0)`
+    }
+    handleFixTitleChange = (currentIndex) => {
+        let item = this.props.data[currentIndex];
+        this.fixedTitle = item ? item.title : '';
     }
     refresh = () => {
         this.listview.refresh()
@@ -129,7 +149,16 @@ export default class ListView extends Component {
                         )
                     }
                 </ul>
-                <div className="list-shortcut">
+                {
+                    this.state.scrollY < 0
+                    ? (
+                        <div className="list-fixed" ref={topFixed => this.topFixed = topFixed}>
+                            <div className="fixed-title">{this.fixedTitle}</div>
+                        </div>
+                      )
+                    : '' 
+                }
+                <div className="list-shortcut" onTouchStart={this.shortcutToggle}>
                     <ul>
                         {
                             this.state.shortcutList.map((item, index) =>
