0af34ee21456b69bdeed7edb02c211a34518620a
Administrator@XTM-01701112049 <502583304@qq.com>
2018-02-16 14:31:47 +0800

    修复加载图bug

--- src/base/loading/loading.js
+++ src/base/loading/loading.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './loading.less';
+import loadingImg from 'common/images/loading.gif'
 
 export default class Loading extends Component {
     render() {
         return (
             <div className="loading">
-                <img width="24" height="24" src="./loading.gif" alt=""/>
+                <img width="24" height="24" src={loadingImg} alt=""/>
                 <p className="desc">{this.props.title}</p>
             </div>
         );
