d1cee61d8d3c9b687b04833f5861e068812b0586
Administrator@XTM-01701112049 <502583304@qq.com>
2018-02-18 11:54:22 +0800

    页面骨架

--- src/components/rank/rank.js
+++ src/components/rank/rank.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
+import Scroll from 'base/scroll/scroll'
+import Loading from 'base/loading/loading'
+import {getTopList} from 'api/rank'
 
 export default class RouterConfig extends Component {
+    state = {
+        topList: []
+    }
     render() {
         return (
-            <div>
-                rank
+            <div className="rank" ref={rank => this.rank = rank}>
+                <Scroll>
+                    <ul>
+
+                    </ul>
+                    {
+                        !this.state.topList.length
+                        ? <div className="loading-container">
+                            <Loading />
+                          </div>
+                        : ''
+                    }
+                    
+                </Scroll>
             </div>
         );
     }
