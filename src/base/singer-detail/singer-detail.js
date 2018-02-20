8d0f7123c61da3d54a025113b1fdbb90575ba642
Administrator@XTM-01701112049 <502583304@qq.com>
2018-02-20 10:13:08 +0800

    编程式设置路由

--- src/base/singer-detail/singer-detail.js
+++ src/base/singer-detail/singer-detail.js
@@ -3,6 +3,9 @@ import { getSingerDetail } from 'api/singer';
 import { ERR_OK } from 'api/config';
 import { createSong } from 'common/js/song';
 import MusicList from 'components/music-list/music-list';
+import createHistory from "history/createBrowserHistory";
+
+const history = createHistory();
 
 export default class SingerDetail extends Component {
     state = {
@@ -12,14 +15,11 @@ export default class SingerDetail extends Component {
     }
     _getSingerDetail() {
         if (!this.singer.id) {
-          this.$router.push({
-            path: '/singer'
-          })
-          return
+          history.push('/singer')
+          return;
         }
         getSingerDetail(this.singer.id).then((res) => {
           if (res.code === ERR_OK) {
-            this.songs = this._normalizeSongs(res.data.list)
             this.setState({songs: this._normalizeSongs(res.data.list)})
           }
         }, (err) => {
@@ -32,7 +32,7 @@ export default class SingerDetail extends Component {
           let { musicData } = item
           ret.push(createSong(musicData))
         })
-        return ret
+        return ret;
     }
     render() {
         return (
