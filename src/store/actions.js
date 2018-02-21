98d4c62dd109ccec8d3e2e063f428315c2987a9a
Administrator@XTM-01701112049 <502583304@qq.com>
2018-02-21 18:08:48 +0800

    设置歌手

---
+++ src/store/actions.js
@@ -0,0 +1,6 @@
+import * as types from './action-types';
+
+export const setSinger = singer => ({
+    type: types.SET_SINGER,
+    singer
+})
---
+++ src/store/reducers/list.js
@@ -0,0 +1,17 @@
+import * as types from '../action-type';
+
+const defaultState = {
+    singer: {}
+}
+
+export const singer = (state, action) => {
+    switch(action.type) {
+        case types.SET_SINGER:
+            return {
+                ...state,
+                singer: action.singer
+            };
+        default: 
+            return state;
+    }
+}
