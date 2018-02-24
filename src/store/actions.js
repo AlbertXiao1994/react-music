84e2f451591aa618627985300bde1e92180a4e4d
Administrator@XTM-01701112049 <502583304@qq.com>
2018-02-25 00:23:30 +0800

    删去调试打印

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,12 +1,13 @@
 import * as types from './action-types';
 
-const receiveSinger = singer => ({
-    type: types.SET_SINGER,
-    singer
-})
+const receiveSinger = (singer) => {
+    return {
+        type: types.SET_SINGER,
+        singer
+    }
+}
 
 export const setSinger = (singer) => dispatch => {
-    console.log(singer)
     dispatch(receiveSinger(singer))
 }
 
