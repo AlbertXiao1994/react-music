530e1e6d206c9f1dbd4a8ae66df55db29179350c
Administrator@XTM-01701112049 <502583304@qq.com>
2018-02-25 22:03:38 +0800

    增加SearchList组件

---
+++ src/base/search-list/search-list.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import { is, fromJS } from 'immutable';
+import './search-list.less';
+import PropTypes from 'prop-types';
+
+export default class SearchList extends Component {
+    shouldComponentUpdate(nextProps, nextState) {
+        return !is(fromJS(this.props), fromJS(nextProps)) || !is(fromJS(this.state),fromJS(nextState))
+    }
+    selectItem(item) {
+        this.props.select(item)
+    }
+    deleteIteme(e, item) {
+        e.stopPropagation();
+        this.props.delete(item)
+    }
+    render() {
+        return (
+            <div className="search-list">
+            {
+                this.props.history.length
+                ? <ul>
+                    {
+                        this.props.history.map((item, index) =>
+                            <li key={index} className="search-item" onClick={()=>this.selectItem(item)}>
+                                <span className="text">{item}</span>
+                                <span className="icon" onClick={(e)=>this.deleteItem(e, item)}>
+                                    <i className="icon-delete"></i>
+                                </span>
+                            </li>
+                        )
+                    }
+                  </ul>
+                : ''
+            }
+            </div>
+        );
+    }
+}
+
+SearchList.propTypes = {
+    history: PropTypes.array
+};
+
+SearchList.defaultProps = {
+    history: []
+}
