9b90a1b981bae5e4efc07d2c6f55059b82ead469
Administrator@XTM-01701112049 <502583304@qq.com>
2018-02-25 19:24:07 +0800

    update

--- src/components/suggest/suggest.js
+++ src/components/suggest/suggest.js
@@ -8,16 +8,64 @@ import Scroll from 'base/scroll/scroll';
 import Loading from 'base/loading/loading';
 import NoResult from 'base/no-result/no-result';
 import Singer from 'common/js/singer';
+import PropTypes from 'prop-types';
 
 export default class Suggest extends Component {
+    state = {
+        result: [],
+        hasMore: false
+    };
     shouldComponentUpdate(nextProps, nextState) {
         return !is(fromJS(this.props), fromJS(nextProps)) || !is(fromJS(this.state),fromJS(nextState))
     }
     render() {
+        const { result, hasMore } = this.state;
         return (
-            <div>
-                
-            </div>
+            <Scroll 
+                ref={scroll=>this.scroll=scroll}
+                className="suggest"
+                data={result}
+                scrollToEnd="searchMore" 
+                pullup="pullup"
+                beforeScroll="listScroll"
+                // :beforeScroll="beforeScroll"
+            >
+                <ul class="suggest-list">
+                    {
+                        result.map((item,index) => (
+                                !(item.type&&index!==0)
+                                ? <li className="suggest-item" onClick={()=>this.selectItem(item)}>
+                                    <div className="icon">
+                                        <i className="getIconCls(item)"></i>
+                                    </div>
+                                    <div className="name">
+                                        <p className="text" dangerouslySetInnerHTML={()=>this.getText(item)}></p>
+                                    </div>
+                                  </li>
+                                : ''
+                            )
+                        )
+                    }
+                    {hasMore ? <Loading /> : ''}
+                </ul>
+                {
+                    !hasMore&&!result.length
+                    ? <div className="no-result-wrapper">
+                        <NoResult title="抱歉，暂无搜索结果"></NoResult>
+                      </div>
+                    : ''
+                }
+        </Scroll>
         );
     }
+}
+
+Suggest.propTypes = {
+    query: PropTypes.string,
+    showSinger: PropTypes.bool
+}
+
+Suggest.defaultProps = {
+    query: '',
+    showSinger: true
 }
