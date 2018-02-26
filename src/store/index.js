69ac0ebf0cd0d4a97ac20fc0a21fce9d4107b82c
Administrator@XTM-01701112049 <502583304@qq.com>
2018-02-26 16:59:46 +0800

    使用redux-logger中间件

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,16 @@
 import { createStore, applyMiddleware } from 'redux';
 import reducer from './reducers';
 import thunk from 'redux-thunk';
+import { createLogger } from 'redux-logger';
+
+const middleware = [ thunk ];
+if (process.env.NODE_ENV !== 'production') {
+  middleware.push(createLogger());
+}
 
 const store = createStore(
-    reducer,
-    applyMiddleware(thunk)
+  reducer,
+  applyMiddleware(...middleware)
 )
 
 export default store
