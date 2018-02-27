8bd1493f8427447e2f623729dbf770d6bbcb0f97
Administrator@XTM-01701112049 <502583304@qq.com>
2018-02-27 14:08:10 +0800

    设置Rank组件子路由

--- src/components/rank/rank.js
+++ src/components/rank/rank.js
@@ -4,6 +4,8 @@ import Loading from 'base/loading/loading';
 import { getTopList } from 'api/rank';
 import { is, fromJS } from 'immutable';
 import './rank.less';
+import { Route } from 'react-router-dom';
+import TopList from 'components/top-list/top-list';
 
 export default class RouterConfig extends Component {
     state = {
@@ -13,7 +15,8 @@ export default class RouterConfig extends Component {
         this._getTopList()
     }
     selectItem = (item) => {
-
+        this.props.history.push(`/rank/${item.topID}`)
+        // this.props.setTopList(item)
     }
     shouldComponentUpdate(nextProps, nextState) {
         return !is(fromJS(this.props), fromJS(nextProps)) || !is(fromJS(this.state),fromJS(nextState))
@@ -28,43 +31,50 @@ export default class RouterConfig extends Component {
     render() {
         return (
             <div className="rank" ref={rank => this.rank = rank}>
-                <Scroll 
-                    className="toplist"
-                    ref={topList => this.topList = topList}
-                    data={this.state.topList}>
-                    <ul>
-                        {
-                            this.state.topList.map((list, id) =>
-                                list.topID!==201
-                                ? (<li className="item" onClick={this.selectItem(list)} key={id}>
-                                    <div className="icon">
-                                        <img src={list.pic_v12} width="100" height="100" alt=""/>
-                                    </div>
-                                    <ul className="songlist">
-                                        {
-                                            list.songlist.map((song, index) =>
-                                                index < 3
-                                                ? <li className="song" key={index}>
-                                                    <span>{index+1}</span>
-                                                    <span>{song.songname}-{song.singername}</span>
-                                                  </li>
-                                                : ''
-                                            )
-                                        }
-                                    </ul>
-                                  </li>)
+                <Route path={`${this.props.match.url}/:id`} component={TopList} />
+                <Route
+                    exact
+                    path={this.props.match.url}
+                    render={() => 
+                        <Scroll 
+                            className="toplist"
+                            ref={topList => this.topList = topList}
+                            data={this.state.topList}>
+                            <ul>
+                                {
+                                    this.state.topList.map((list, id) =>
+                                        list.topID!==201
+                                        ? (<li className="item" onClick={()=>this.selectItem(list)} key={id}>
+                                            <div className="icon">
+                                                <img src={list.pic_v12} width="100" height="100" alt=""/>
+                                            </div>
+                                            <ul className="songlist">
+                                                {
+                                                    list.songlist.map((song, index) =>
+                                                        index < 3
+                                                        ? <li className="song" key={index}>
+                                                            <span>{index+1}</span>
+                                                            <span>{song.songname}-{song.singername}</span>
+                                                        </li>
+                                                        : ''
+                                                    )
+                                                }
+                                            </ul>
+                                        </li>)
+                                        : ''
+                                    )
+                                }
+                            </ul>
+                            {
+                                !this.state.topList.length
+                                ? <div className="loading-container">
+                                    <Loading /> 
+                                </div>
                                 : ''
-                            )
-                        }
-                    </ul>
-                    {
-                        !this.state.topList.length
-                        ? <div className="loading-container">
-                            <Loading /> 
-                          </div>
-                        : ''
+                            }
+                        </Scroll>
                     }
-                </Scroll>
+                />
             </div>
         );
     }
