a78f366b129ab4e2047e20159658b492eff8eaf2
Administrator@XTM-01701112049 <502583304@qq.com>
2018-02-27 11:41:28 +0800

    增加播放历史、收藏相关action

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,5 +1,6 @@
 import * as types from './action-types';
 import { saveSearch, clearSearch, deleteSearch } from 'common/js/cache';
+import { savePlay, saveFavorite, deleteFavorite } from 'common/js/cache';
 
 const receiveSinger = (singer) => {
     return {
@@ -12,6 +13,20 @@ const receiveSearchHistory = (searchHistory) => {
     return {
         type: types.SET_SEARCH_HISTORY,
         searchHistory
+    }
+}
+
+const receivePlayHistory = (playHistory) => {
+    return {
+        type: types.SET_PLAY_HISTORY,
+        playHistory
+    }
+}
+
+const receiveFavoriteList = (favoriteList) => {
+    return {
+        type: types.SET_FAVORITE_LIST,
+        favoriteList
     }
 }
 
@@ -32,5 +47,20 @@ export const clearSearchHistory = () => dispatch => {
 export const deleteSearchHistory = (query) => dispatch => {
     let searchHistory = deleteSearch(query)
     dispatch(receiveSearchHistory(searchHistory))
+}
+
+export const saveFavoriteList = (song) => dispatch => {
+    let favoriteList = saveFavorite(song)
+    dispatch(receiveFavoriteList(favoriteList))
+}
+
+export const savePlayHistory = (song) => dispatch => {
+    let playHistory = savePlay(song)
+    dispatch(receivePlayHistory(playHistory))
+}
+
+export const deleteFavoriteList = (song) => dispatch => {
+    let favoriteList = deleteFavorite(song)
+    dispatch(receiveFavoriteList(favoriteList))
 }
 
