15fcbeef9952c470fd4b55f9cd5b0db6b9755c62
Administrator@XTM-01701112049 <502583304@qq.com>
2018-02-27 14:36:27 +0800

    将Recommend组件拆分成UI和容器组件

--- src/components/recommend/recommend.js
+++ src/components/recommend/recommend.js
@@ -6,9 +6,12 @@ import { getRecommend, getDiscList } from 'api/recommend';
 import { ERR_OK } from 'api/config';
 import Loading from 'base/loading/loading';
 import { is, fromJS } from 'immutable';
-// import { Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import Disc from 'components/disc/disc';
+import { connect } from 'react-redux';
+import { setDisc } from '@/store/actions';
 
-export default class Recommend extends Component {
+class Recommend extends Component {
   state = {
     recommends: [],
     discList: []
@@ -19,6 +22,16 @@ export default class Recommend extends Component {
   }
   shouldComponentUpdate(nextProps, nextState) {
         return !is(fromJS(this.props), fromJS(nextProps)) || !is(fromJS(this.state),fromJS(nextState))
+  }
+  selectItem = (item) => {
+    this.props.history.push(`/recommend/${item.dissid}`)
+    this.props.setDisc(item)
+  }
+  loadImg = () => {
+    if (!this.checkLoad) {
+      this.scroll.refresh()
+      this.checkLoad = true
+    }
   }
   _getRecommend() {
     getRecommend().then((res) => {
@@ -38,66 +51,75 @@ export default class Recommend extends Component {
       console.log(err)
     })
   }
-  loadImg = () => {
-    if (!this.checkLoad) {
-      this.scroll.refresh()
-      this.checkLoad = true
-    }
-  }
   render() {
     return (
       <div className="recommend">
-        <Scroll
-          className="recommend-content"
-          ref={(scroll) => {this.scroll = scroll}}
-          data={this.state.discList}
-        >
-          <div>
-            <div className="slider-wrapper">
-              <Slider>
-                {
-                  this.state.recommends.map((item, index) => 
-                    <div key={index}>
-                      <a href={item.linkUrl}>
-                        <img src={item.picUrl} className="needsClick" alt="" />
-                      </a>
+        <Route path={`${this.props.match.url}/:id`} component={Disc} />
+            <Route
+              exact
+              path={this.props.match.url}
+              render={() => 
+                <Scroll
+                  className="recommend-content"
+                  ref={(scroll) => {this.scroll = scroll}}
+                  data={this.state.discList}
+                >
+                  <div>
+                    <div className="slider-wrapper">
+                      <Slider>
+                        {
+                          this.state.recommends.map((item, index) => 
+                            <div key={index}>
+                              <a href={item.linkUrl}>
+                                <img src={item.picUrl} className="needsClick" alt="" />
+                              </a>
+                            </div>
+                          )
+                        }
+                      </Slider>
                     </div>
-                  )
-                }
-              </Slider>
-            </div>
-            <div className="recommend-list">
-              <h1 className="list-title">热门歌单推荐</h1>
-              <ul>
-                  {
-                    this.state.discList.map((item, index) => 
-                      <li className="item" key={index}>
-                        <div className="icon">
-                          <img width="60" height="60" src={item.imgurl} alt={index} />
-                        </div>
-                        <div className="text">
-                          <h2 className="title">{item.dissname}</h2>
-                          <p className="creator">{item.creator.name}</p>
-                        </div>
-                      </li>
-                    )
-                  }
-              </ul>
-            </div>
-          </div>
-          <div className="loading-container">
-              {
-                  !this.state.discList.length
-                  ? <Loading />
-                  : ''
+                    <div className="recommend-list">
+                      <h1 className="list-title">热门歌单推荐</h1>
+                      <ul>
+                          {
+                            this.state.discList.map((item, index) => 
+                              <li 
+                                className="item"
+                                key={index}
+                                onClick={()=>this.selectItem(item)}
+                              >
+                                <div className="icon">
+                                  <img width="60" height="60" src={item.imgurl} alt={index} />
+                                </div>
+                                <div className="text">
+                                  <h2 className="title">{item.dissname}</h2>
+                                  <p className="creator">{item.creator.name}</p>
+                                </div>
+                              </li>
+                            )
+                          }
+                      </ul>
+                    </div>
+                  </div>
+                  <div className="loading-container">
+                      {
+                          !this.state.discList.length
+                          ? <Loading />
+                          : ''
+                      }
+                  </div>
+                </Scroll>
               }
-          </div>
-        </Scroll>
-        {/* <Route
-          path={`${this.props.match.url}/:Id`}
-          component={Topic}
-        /> */}
+            />
       </div>
     );
   }
 }
+
+const mapStateToProps = () => ({})
+
+const mapDispatchToProps =  {
+  setDisc
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Recommend)
