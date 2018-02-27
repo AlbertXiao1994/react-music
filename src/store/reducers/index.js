fef3322ddd59a0045ecb2e7eb4570bc5ca5fe072
Administrator@XTM-01701112049 <502583304@qq.com>
2018-02-27 14:11:09 +0800

    update

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,19 +1,18 @@
 import { combineReducers } from 'redux';
-import { singer } from './list';
+import list from './list';
 import history from './history';
 import * as player from './player';
 import * as fromList from './list';
 import * as fromHistory from './history';
 
 export default combineReducers({
-    singer,
-    // list,
+    list,
     history,
     ...player
 })
 
 export const getSinger = (state) => {
-    return fromList.getSinger(state.singer)
+    return fromList.getSinger(state.list)
 }
 
 export const getSearchHistory = (state) => {
