1ac8000a5c9ccd07418503302869075e2f359bea
Administrator@XTM-01701112049 <502583304@qq.com>
2018-02-28 15:01:40 +0800

    实现路由按需加载

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import Recommend from 'components/recommend/recommend';
-import Singer from 'components/singer/singer';
-import Rank from 'components/rank/rank';
-import Search from 'components/search/search';
+// import Singer from 'components/singer/singer';
+// import Rank from 'components/rank/rank';
+// import Search from 'components/search/search';
 import MHeader from 'components/m-header/m-header';
 import Tab from 'components/tab/tab';
-import Player from 'components/player/player'
+import Player from 'components/player/player';
+import asyncComponent from 'components/AsyncComponent';
+// import Loadable from 'react-loadable';
+// import Loading from 'base/loading/loading';
+
+// const Singer = Loadable({
+//   loader: () => import('components/singer/singer'),
+//   loading: Loading
+// })
+
+// const Rank = Loadable({
+//   loader: () => import('components/rank/rank'),
+//   loading: Loading
+// })
+
+// const Search = Loadable({
+//   loader: () => import('components/search/search'),
+//   loading: Loading
+// })
+
+const Singer = asyncComponent(() => import("components/singer/singer"));
+const Rank = asyncComponent(() => import("components/rank/rank"));
+const Search = asyncComponent(() => import("components/search/search"));
 
 class App extends Component {
   render() {
