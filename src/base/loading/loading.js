35aaa816efca90d6245fb98048c86f5369a63c77
Administrator@XTM-01701112049 <502583304@qq.com>
2018-03-30 00:06:29 +0800

    将Loading组件改为无状态组件

--- src/base/loading/loading.js
+++ src/base/loading/loading.js
@@ -1,18 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import './loading.less';
 import loadingImg from 'common/images/loading.gif'
 
-export default class Loading extends Component {
-    render() {
-        return (
-            <div className="loading">
-                <img width="24" height="24" src={loadingImg} alt=""/>
-                <p className="desc">{this.props.title}</p>
-            </div>
-        );
-    }
-}
+const Loading = (props) => (
+    <div className="loading">
+        <img width="24" height="24" src={loadingImg} alt=""/>
+        <p className="desc">{props.title}</p>
+    </div>
+)
 
 Loading.protoTypes = {
     title: PropTypes.string
@@ -21,3 +17,5 @@ Loading.protoTypes = {
 Loading.defaultProps = {
     title: "正在载入..."
 }
+
+export default Loading;
