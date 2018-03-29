9a48eda15182880def42dd497e7078980b5a49a6
Administrator@XTM-01701112049 <502583304@qq.com>
2018-03-29 23:14:31 +0800

    将Tab组件修改为无状态组件

--- src/components/tab/tab.js
+++ src/components/tab/tab.js
@@ -1,28 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { is, fromJS } from 'immutable';
 import './tab.less';
 
-export default class Tab extends Component {
-    shouldComponentUpdate(nextProps, nextState) {
-        return !is(fromJS(this.props), fromJS(nextProps)) || !is(fromJS(this.state),fromJS(nextState))
-    }
-    render() {
-        return (
-            <div className="tab">
-                <NavLink className="tab-item" activeClassName="router-link-active" to="/recommend">
-                   <span className="tab-link">推荐</span>
-                </NavLink>
-                <NavLink className="tab-item" activeClassName="router-link-active" to="/singer">
-                   <span className="tab-link">歌手</span>
-                </NavLink>
-                <NavLink className="tab-item" activeClassName="router-link-active" to="/rank">
-                   <span className="tab-link">排名</span>
-                </NavLink>
-                <NavLink className="tab-item" activeClassName="router-link-active" to="/search">
-                   <span className="tab-link">搜索</span>
-                </NavLink>
-            </div>
-        );
-    }
-}
+const Tab = () => (
+    <div className="tab">
+        <NavLink className="tab-item" activeClassName="router-link-active" to="/recommend">
+            <span className="tab-link">推荐</span>
+        </NavLink>
+        <NavLink className="tab-item" activeClassName="router-link-active" to="/singer">
+            <span className="tab-link">歌手</span>
+        </NavLink>
+        <NavLink className="tab-item" activeClassName="router-link-active" to="/rank">
+            <span className="tab-link">排名</span>
+        </NavLink>
+        <NavLink className="tab-item" activeClassName="router-link-active" to="/search">
+            <span className="tab-link">搜索</span>
+        </NavLink>
+    </div>
+)
+
+export default Tab;
