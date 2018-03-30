401478670ee15e0110763d49297769d759e5d819
Administrator@XTM-01701112049 <502583304@qq.com>
2018-03-30 19:20:06 +0800

    去除fromJS，减小开销，直接比较state、props

--- src/base/progress-circle/progress-circle.js
+++ src/base/progress-circle/progress-circle.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { is, fromJS } from 'immutable';
 import './progress-circle.less';
 import PropTypes from 'prop-types';
 
@@ -11,20 +10,42 @@ export default class ProgressCircle extends Component {
         this.dashArray = Math.PI * 100
     }
     shouldComponentUpdate(nextProps, nextState) {
-        if (nextProps.percent !== this.props.percent) {
+        const thisProps = this.props || {};
+        const thisState = this.state || {};
+        nextState = nextState || {};
+        nextProps = nextProps || {};
+
+        if (nextProps.percent !== thisProps.percent) {
             this.setState({dashOffset: this.dashArray * (1 - nextProps.percent)})
         }
-        return !is(fromJS(this.props), fromJS(nextProps)) || !is(fromJS(this.state),fromJS(nextState))
+
+        if (Object.keys(thisProps).length !== Object.keys(nextProps).length ||
+            Object.keys(thisState).length !== Object.keys(nextState).length) {
+            return true;
+        }
+
+        for (const key in nextProps) {
+            if (!Object.is(thisProps[key], nextProps[key])) {
+                return true;
+            }
+        }
+
+        for (const key in nextState) {
+            if (!Object.is(thisState[key], nextState[key])) {
+                return true;
+            }
+        }
+        return false;
     }
     render() {
-        const { radius} = this.props;
+        const { radius, children } = this.props;
         return (
             <div className="progress-circle">
                 <svg width={radius} height={radius} viewBox="0 0 100 100" version="1.1" xmlns="http://www.w3.org/2000/svg">
                     <circle className="progress-background" r="50" cx="50" cy="50" fill="transparent"/>
                     <circle className="progress-bar" r="50" cx="50" cy="50" fill="transparent" stroke-dasharray={this.dashArray} stroke-dashoffset={this.state.dashOffset}/>
                 </svg>
-                {this.props.children}
+                {children}
             </div>
         );
     }
