8bbaba7ca7f0cb9c49b44a7a4b3f97a02110e80f
Administrator@XTM-01701112049 <502583304@qq.com>
2018-03-30 19:40:19 +0800

    改成无状态组件

--- src/components/m-header/m-header.js
+++ src/components/m-header/m-header.js
@@ -1,21 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import './m-header.less';
-import { is, fromJS } from 'immutable';
 
-export default class MHeader extends Component {
-    shouldComponentUpdate(nextProps, nextState) {
-        return !is(fromJS(this.props), fromJS(nextProps)) || !is(fromJS(this.state),fromJS(nextState))
-    }
-    render() {
-        return (
-            <div className="m-header">
-                <div className="icon"></div>
-                <h1 className="text">Albert Music</h1>
-                <Link to="/user" className="mine">
-                    <i className="icon-mine" />
-                </Link>
-            </div>
-        );
-    }
-}
+const MHeader = () => (
+    <div className="m-header">
+        <div className="icon"></div>
+            <h1 className="text">Albert Music</h1>
+            <Link to="/user" className="mine">
+                <i className="icon-mine" />
+            </Link>
+    </div>
+)
+
+export default MHeader;
