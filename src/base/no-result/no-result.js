d76242054ec3bbe5a5a82520ef00d6b28de16922
Administrator@XTM-01701112049 <502583304@qq.com>
2018-03-30 19:17:30 +0800

    改成无状态组件

--- src/base/no-result/no-result.js
+++ src/base/no-result/no-result.js
@@ -1,21 +1,13 @@
-import React, { Component } from 'react';
-import { is, fromJS } from 'immutable';
+import React from 'react';
 import PropTypes from 'prop-types';
 import './no-result.less';
 
-export default class NoResult extends Component {
-    shouldComponentUpdate(nextProps, nextState) {
-        return !is(fromJS(this.props), fromJS(nextProps)) || !is(fromJS(this.state),fromJS(nextState))
-    }
-    render() {
-        return (
-            <div className="no-result">
-                <div className="no-result-icon"></div>
-                <p className="no-result-text">{this.props.title}</p>
-            </div>
-        );
-    }
-}
+const NoResult = (props) => (
+    <div className="no-result">
+        <div className="no-result-icon"></div>
+            <p className="no-result-text">{props.title}</p>
+    </div>
+)
 
 NoResult.propTypes = {
     title: PropTypes.string
@@ -24,3 +16,5 @@ NoResult.propTypes = {
 NoResult.defaultProps = {
     title: ''
 }
+
+export default NoResult;
